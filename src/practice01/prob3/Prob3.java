5b0834e740535d0ab03d5054258dad5cf87cf417
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-06 17:11:44 +0900

    3번문제

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,27 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요 : ");
+		
+		int x = scanner.nextInt();
+		int sum = 0;
+		
+		if(x%2==0) {
+			for(int i=0; i<=x; ++i) {
+				if(i%2==0)
+					sum += i;
+			}
+		}else {
+			for(int i=0; i<=x; ++i) {
+				if(i%2==1)
+					sum += i;
+			}
+		}
+		System.out.println("결과값 : " + sum);
 	}
 }
