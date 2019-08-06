ecca5543641b94ee08b922cb34345ea594d0da05
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-06 17:56:58 +0900

    4번문제

--- src/practice01/prob4/Prob4.java
+++ src/practice01/prob4/Prob4.java
@@ -1,6 +1,19 @@
 package practice01.prob4;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("문자열 입력 : ");
+		String input = scanner.nextLine();
+		
+		for(int i=0; i<=input.length()-1; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print(input.charAt(j));
+			}
+			System.out.println();
+		}
 	}
 }
